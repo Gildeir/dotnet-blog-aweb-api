4b5b46eacc4454a43fd442b385354b29d0b40d40
Gildeir L.Rodrigues <gildeirlopes@gmail.com>
2023-02-15 20:25:18 -0300

    Finishing standards excepction handling and implementing extension GetErros modelstate

---
+++ Extensions/ModelStateExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BlogWebApi.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErros(this ModelStateDictionary modelstate)
+        {
+            var result = new List<string>();
+            
+            foreach (var item in modelstate.Values)
+            {
+                result.AddRange(item.Errors.Select(error => error.ErrorMessage));
+
+                //foreach (var error in item.Errors)
+                //{
+                //    result.Add(error.ErrorMessage);
+                //}
+            }
+
+            return result;
+        }
+    }
+}
